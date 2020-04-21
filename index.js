0d8f359585802dbe2b5ffeecf8004919f84139fe
Robert Howden <robert.howden@current-rms.com>
2020-04-21 10:19:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function doToElementsInArray(array, callback) {
 
 
 function changeCompletely(element, index, array) {
+  debugger;
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
